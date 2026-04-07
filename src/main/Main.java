d85d2ecf9323292e2827b766d605c77d7f234ba8
Thalita <thalitaoliveira473@gmail.com>
2026-04-06 23:57:31 -0300

    Add files via upload

---
+++ src/main/Main.java
@@ -0,0 +1,3 @@
+package main;
+
+import model.Produto;
---
+++ src/model/Produto.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Produto {
+    private String nome; // String-texto
+    private double preco; // double-numero quebrado
+    private int quantidade; //int-numero inteiro
+    private int codigo; //int-numero inteiro
+}
